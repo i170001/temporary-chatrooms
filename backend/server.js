41fd10f5a35ba33ea8fb7ac13c2f1bc45a033e26
i170001 <49742549+i170001@users.noreply.github.com>
2024-08-31 07:25:46 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  origin: 'https://temporary-chatrooms.onrender.com/',
+  credentials: true,
+}));
 app.use(securityMiddleware.checkJWT);
 
 require('dotenv').config();
