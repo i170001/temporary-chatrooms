8163b18cffaa6285432c229f691ee342a6d62ed3
i170001 <49742549+i170001@users.noreply.github.com>
2024-08-31 09:40:50 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import vitePluginRequire from "vite-plugin-require";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginRequire()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
